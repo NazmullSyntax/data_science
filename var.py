098f64a658a4a45fec3ae766d95c854a50190a12
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-01-09 19:54:30 +0600

    just basic paython varriable and string

---
+++ var.py
@@ -0,0 +1,25 @@
+# x=4
+# print(x)
+# y='nazmul'
+# print(y)
+# # varriable conversion
+# a=str(3)
+# b=int(4)
+# c=float(5)
+# print(type(a))
+# print(b)
+# print(c)
+# now see the legal variable
+myvar='somu'
+my_var="nazu"
+_myvar='love'
+print(myvar)
+print(my_var)
+#string name plus kora
+first_name='sadia'
+mid_name='apni'
+print(first_name + "  " +mid_name)
+# string concatination operer ta
+x=str(5)
+y="nazmul"
+print(x+y)
